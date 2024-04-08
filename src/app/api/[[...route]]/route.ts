98c91b7fef1e5cb5527007ed5daae5ccb40dace2
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-04-08 03:14:30 +0300

    * Extended route functionality to work with Redis's db

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,61 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { env } from 'hono/adapter';
+import { Redis } from '@upstash/redis';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app.get('/search', (c) => {
-  return c.json({})
+type EnvConfig = {
+  UPSTASH_REDIS_REST_URL: string,
+  UPSTASH_REDIS_REST_TOKEN: string
+}
+
+app.get('/search', async (c) => {
+  try {
+    const { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = env<EnvConfig>(c)
+
+    const start = performance.now()
+
+    const redis = new Redis({
+      url: UPSTASH_REDIS_REST_URL,
+      token: UPSTASH_REDIS_REST_TOKEN
+    })
+
+    const query = c.req.query("q")
+
+    if (!query) return c.json(
+      { message: 'Invalid search query' },
+      { status: 400 }
+    )
+
+    const res = []
+    const rank = await redis.zrank('Dishes', query)
+
+    if (rank !== null && rank !== undefined) {
+      const dish = await redis.zrange<string[]>("Dishes", rank, rank + 100)
+
+      for (const el of dish) {
+        if (!el.startsWith(query)) break
+
+        if (el.endsWith('*')) res.push(el.substring(0, el.length - 1))
+      }
+    }
+
+    const end = performance.now()
+
+    return c.json({
+      result: res,
+      duration: end - start
+    })
+  } catch (err) {
+    console.error(err)
+    return c.json(
+      { result: [], message: 'Internal server error' },
+      { status: 500 }
+    )
+  }
 })
 
 export const GET = handle(app)
