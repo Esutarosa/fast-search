4fbbefa916a460c4aac8b6c114b543d496b1dbcc
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-04-08 19:28:29 +0300

    + Added home page image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image"
 import { useEffect, useState } from "react"
 
 const Home = () => {
@@ -22,7 +23,18 @@ const Home = () => {
   return (
     <main className="h-screen w-screen grainy">
       <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5'">
-        <h1 className='text-5xl tracking-tight font-bold'>SpeedSearch ⚡</h1>
+        <Image
+          src="/home.png"
+          width={200}
+          height={200}
+          alt="Home page dishes"
+          className="cover"
+        />
+
+        <h1 className='text-5xl tracking-tight font-bold flex items-center justify-center'>
+          Speed Search
+        </h1>
+
         <p className='text-zinc-600 text-lg max-w-prose text-center px-4'>
           A high-performance API built with Hono, Next.js and Cloudflare. Type a query below and get your results in miliseconds.
         </p>
